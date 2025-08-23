4f3ee3858b0ab7c2d15e51416748053ed3ed753d
SHLapi <shalabi.74.a@gmail.com>
2025-08-23 21:41:41 +0300

    Added Styles

---
+++ app.js
@@ -0,0 +1,6 @@
+function openDialog(){
+  const dialog = document.getElementById('noteDialog');
+  const titleInput = document.getElementById('noteTitle');
+  const contentInput = document.getElementById('noteContent');
+  dialog.showModal()
+}
