ffac68889fbbf15d6139486fa7e9f6dfb24de1b7
SHLapi <shalabi.74.a@gmail.com>
2025-08-24 22:30:17 +0300

    Fix theme toggle button selection and enhance note generation functionality

--- app.js
+++ app.js
@@ -1,12 +1,37 @@
 const dialog = document.getElementById('noteDialog');
 const titleInput = document.getElementById('noteTitle');
 const contentInput = document.getElementById('noteContent');
-const themeBtn = document.querySelector('themeToggleBtn');
+const themeBtn = document.getElementById('themeToggleBtn');
 const addNoteForm = document.getElementById('noteForm');
-const noteContainer = document.getElementById('noteGrid')
+const noteContainer = document.getElementById('notesGrid')
 
 let Notes = [];
 
+const generateNotes = ()=>  {
+  noteContainer.innerHTML = Notes.map(note => `
+    <div class="noteCard">
+      <h4 class="noteTitle">${note.title}</h4>
+      <p class="noteContent">${note.content}</p>
+      <div class="noteActions">
+        <button type="button" class="editBtn" onclick="clearAllBtn()"><i class="fa fa-pencil" aria-hidden="true"></i></button>
+        <button type="button" class="removeNote" onclick="removeNote(${note.id})" ><i class="fa fa-trash" aria-hidden="true"></i></button>
+      </div>
+    </div>
+    `).join(``)
+    const addNoteBtns = document.createElement('button')
+    addNoteBtns.className = 'addNoteBtn Js'
+    addNoteBtns.textContent = '+ Add Another Note'
+    addNoteBtns.onclick = () => openDialog()
+    noteContainer.appendChild(addNoteBtns)
+}
+const removeNote = (noteId) => {
+  Notes = Notes.filter(note => note.id != noteId) 
+  saveNote();
+  generateNotes();
+}
+const clearAllBtn = () => {
+
+}
 // function to open and close Dialog
 const openDialog = () => {
   dialog.showModal()
@@ -14,12 +39,12 @@ const openDialog = () => {
 const closeDialog = () => {
   dialog.close();
 }
-
-// Change theme and save it
-const Theme = () =>{
+themeBtn.addEventListener('click', () => {
   const dark = document.body.classList.toggle('darkTheme');
   localStorage.setItem('Theme', dark? 'dark' : 'light');
-}
+})
+
+// Change theme and save it
 const applyTheme = ()=>{
   if (localStorage.getItem('Theme') === 'dark'){
     document.body.classList.toggle('darkTheme');
@@ -43,13 +68,19 @@ addNoteForm.addEventListener('submit', (e) =>{
   Notes.push({
     id: noteId(),
     title: Title,
-    content: Content,
+    content: Content
   })
   saveNote()
   closeDialog();
+  generateNotes()
 })
-
+const loadNotes = () => {
+  const savedNotes = localStorage.getItem('Notes')
+  return savedNotes? JSON.parse(savedNotes):[];
+}
 
 document.addEventListener('DOMContentLoaded', ()=> {
   applyTheme();
+  Notes = loadNotes();
+  generateNotes();
 })
