6a096c93a41493a940ad5909e9f040ea533df878
SHLapi <shalabi.74.a@gmail.com>
2025-08-25 12:45:29 +0300

    added clear all notes function and Edit Note Function

--- app.js
+++ app.js
@@ -6,14 +6,14 @@ const addNoteForm = document.getElementById('noteForm');
 const noteContainer = document.getElementById('notesGrid')
 
 let Notes = [];
-
+let editedNoteId = null ;
 const generateNotes = ()=>  {
   noteContainer.innerHTML = Notes.map(note => `
     <div class="noteCard">
       <h4 class="noteTitle">${note.title}</h4>
       <p class="noteContent">${note.content}</p>
       <div class="noteActions">
-        <button type="button" class="editBtn" onclick="clearAllBtn()"><i class="fa fa-pencil" aria-hidden="true"></i></button>
+        <button type="button" class="editBtn" onclick="openDialog(${note.id})"><i class="fa fa-pencil" aria-hidden="true"></i></button>
         <button type="button" class="removeNote" onclick="removeNote(${note.id})" ><i class="fa fa-trash" aria-hidden="true"></i></button>
       </div>
     </div>
@@ -29,16 +29,15 @@ const removeNote = (noteId) => {
   saveNote();
   generateNotes();
 }
-const clearAllBtn = () => {
-
+const clearAllNotes = () => {
+  Notes.length === 0? console.log('all cleared')
+  :localStorage.removeItem('Notes');
+  Notes = [];
+  saveNote();
+  generateNotes();
 }
 // function to open and close Dialog
-const openDialog = () => {
-  dialog.showModal()
-}
-const closeDialog = () => {
-  dialog.close();
-}
+
 themeBtn.addEventListener('click', () => {
   const dark = document.body.classList.toggle('darkTheme');
   localStorage.setItem('Theme', dark? 'dark' : 'light');
@@ -58,18 +57,45 @@ const noteId = ()=>{
 const saveNote = () => {
   localStorage.setItem('Notes', JSON.stringify(Notes));
 }
-addNoteForm.addEventListener('submit', (e) =>{
+const openDialog = (noteId = null) => {
+  if (noteId != null){
+    document.getElementById('dialogTitle').textContent = 'Edit Note';
+    const editedNote = Notes.find( note => note.id == noteId);
+    editedNoteId = noteId;
+    titleInput.value = editedNote.title;
+    contentInput.value = editedNote.content;
+  }else{
+    document.getElementById('dialogTitle').textContent = 'Add Note';
+    editedNoteId = null;
+    titleInput.value = '';  
+    contentInput.value = '';
+  }
+  dialog.showModal()
+}
+const closeDialog = () => {
+  dialog.close();
+}
+addNoteForm.addEventListener('submit', (e) => {
   e.preventDefault();
   const Title = titleInput.value.trim();
   const Content = contentInput.value.trim();
   if ( Title === '' || Content === ''){
     return closeDialog();
   }
-  Notes.push({
+  else if (editedNoteId){
+    const noteIndex = Notes.findIndex(note => note.id == editedNoteId)
+    Notes[noteIndex] = {
+      ...Notes[noteIndex],
+      title: Title,
+      content : Content,
+    }
+  }else
+    Notes.push({
     id: noteId(),
     title: Title,
-    content: Content
+    content: Content,
   })
+  
   saveNote()
   closeDialog();
   generateNotes()
